a9f42e35d118aa9d27f27f4286f919d3c836ac9a
Uche44 <perpetualuchechukwu808@gmail.com>
2026-09-03 13:44:57 +0100

    add server health check

--- unitrack-backend/unitrack/urls.py
+++ unitrack-backend/unitrack/urls.py
@@ -1,8 +1,15 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
+
+
+def health_check(request):
+    return JsonResponse({"status": "ok", "message": "UniTrack API is running"})
+
 
 urlpatterns = [
+    path('', health_check),
     path('admin/', admin.site.urls),
     path('api/', include('accounts.urls')),
     path('api/', include('projects.urls'))
